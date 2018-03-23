526fe1b77d9e51c7d0c1cfaf9e86d2536ccd3cf9
Austin-Scott <12504656+Austin-Scott@users.noreply.github.com>
2018-03-23 16:47:24 -0500

    Implemented rudimentary structure for the code to come

--- SpaceX-Simulation/main.cpp
+++ SpaceX-Simulation/main.cpp
@@ -2,11 +2,52 @@
 #include <fstream>
 
 #include "Tables.h"
+#include "RealData.h"
+#include "SimulatedData.h"
 
 using namespace std;
 
-int main() {
-	//TODO: add simulation code
+void writeResultsToFiles() {
+	ofstream boosterFile("Booster.txt");
+	if (boosterFile) {
+		boosterFile << listBoosters();
+	}
+	boosterFile.close();
 
+	ofstream flownByFile("flownBy.txt");
+	if (flownByFile) {
+		flownByFile << listflownBys();
+	}
+	flownByFile.close();
+
+	ofstream MissionFile("Mission.txt");
+	if (MissionFile) {
+		MissionFile << listMissions();
+	}
+	MissionFile.close();
+
+	ofstream PayloadFile("Payload.txt");
+	if (PayloadFile) {
+		PayloadFile << listPayloads();
+	}
+	PayloadFile.close();
+
+	ofstream DragonFile("Dragon.txt");
+	if (DragonFile) {
+		DragonFile << listDragons();
+	}
+	DragonFile.close();
+
+	ofstream LaunchSiteFile("LaunchSite.txt");
+	if (LaunchSiteFile) {
+		LaunchSiteFile << listLaunchSites();
+	}
+	LaunchSiteFile.close();
+}
+
+int main() {
+	getRealData();
+	runSimulation();
+	writeResultsToFiles();
 	cleanAllData();
 }
